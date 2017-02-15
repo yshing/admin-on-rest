33f1293f47eab84069c07c277666c64d7cab2477
Yuen Shing <y.shing@me.com>
2017-02-15 13:23:56 +0800

    Handle cases when FieldTitle is a Component

--- src/util/FieldTitle.js
+++ src/util/FieldTitle.js
@@ -5,19 +5,17 @@ import compose from 'recompose/compose';
 
 import translate from '../i18n/translate';
 
-const FieldTitle = ({ resource, source, label, translate }) => (
-    <span>
-        {typeof label === 'string' ?
-            label
-            :
-            (typeof source !== 'undefined' ?
-                translate(`resources.${resource}.fields.${source}`, { _: inflection.humanize(source) })
-                :
-                ''
-            )
-        }
-    </span>
-);
+const FieldTitle = ({ resource, source, label, translate }) => {
+    switch (typeof label) {
+    case 'string':
+        return (<span>{label}</span>);
+    case 'undefined':
+        if (source) return (<span>{translate(`resources.${resource}.fields.${source}`, { _: inflection.humanize(source) })}</span>);
+        return (<span />);
+    default:
+        return React.cloneElement(label, { resource, source, translate });
+    }
+};
 
 FieldTitle.propTypes = {
     resource: PropTypes.string,
